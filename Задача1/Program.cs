3d25ac2cdb48305fa64b4206d0e58944178bc4f7
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-27 01:01:00 +0500

    Задачи к семинару 2 домашняя работа

---
+++ Задача1/Program.cs
@@ -0,0 +1,19 @@
+﻿//на вход принимается трехзначное число и выводится вторая цифра числа.
+
+int SecNumber(int number)
+{
+    if (number < 100 || number > 999)
+    {
+        Console.WriteLine("Wrong Number");
+        return 0;
+    }
+    else
+    {
+        int Second = number % 100 / 10;
+        Console.WriteLine(Second);
+        return 0;
+    }
+}
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+SecNumber(num);
---
+++ Задача2_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+// 198 %10=8
+// 1986%100=86/10=8
+// 19865%1000/100=8
+
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int k =  (int)Math.Log10(n)-2;
+// Console.WriteLine(k < 0 ? "No such digit" : (n %(int)Math.Pow(10, k + 1) /(int)Math.Pow(10, k)).ToString());
+
+
+void ThirdNumber(int Number)
+{
+    if (Number < 100) Console.WriteLine("Третьей цифры нет");
+    if (Number >= 100 && Number <= 999) Console.WriteLine(Number % 10);
+    //int res=Number%100/10;
+    if (Number >= 1000 && Number <= 9999) Console.WriteLine(Number % 100 / 10);
+    if (Number >= 10000 && Number <= 99999) Console.WriteLine(Number % 1000 / 100);
+    if (Number >= 100000 && Number <= 999999) Console.WriteLine(Number % 10000 / 1000);
+    if (Number >= 1000000 && Number <= 9999999) Console.WriteLine(Number % 100000 / 10000);
+    if (Number >= 10000000 && Number <= 99999999) Console.WriteLine(Number % 1000000 / 100000);
+    if (Number >= 100000000 && Number <= 999999999) Console.WriteLine(Number % 10000000 / 1000000);
+    if (Number >= 1000000000 && Number <= 2147483647) Console.WriteLine(Number % 100000000 / 10000000);
+}
+Console.WriteLine("Input number");
+int Res = Convert.ToInt32(Console.ReadLine());
+ThirdNumber(Res);
